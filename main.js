48174be5657034f3fa11fdefdb3fc9ddffc8a334
Barb Page <giggles51810@gmail.com>
2021-12-03 22:12:58 -0500

    finished media queries to make site responsive

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//---------------NAV-----------------
+
+
+
+//hamburgerButton
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+let x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
+
+
+
 //------------VARIABLES-------------
 
 
